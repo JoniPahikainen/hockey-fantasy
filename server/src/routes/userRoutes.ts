ecf5269ec0558fc5b0bf714d31720f80cfff15c9
Joni Pahikainen <joni.pahikainen01@gmail.com>
2025-12-22 19:43:35 +0200

    feat: add login, register and delete user

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createUser, deleteUser } from '../controllers/userController';
+import { createUser, deleteUser, loginUser } from '../controllers/userController';
 
 const router = Router();
 
-router.post('/', createUser);
+router.post('/register', createUser);
 router.delete('/:id', deleteUser);
+router.post('/login', loginUser);
 
 export default router;
 
